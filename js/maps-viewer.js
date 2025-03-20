cd55f38d2ca42cbbc27283d9fcb2d2c6a7b95cb7
RomainRonflette <RomainRonflette@users.noreply.github.com>
2025-03-20 15:10:00 +0000

    deploy: 0e4b8a3f589b76396085d15e174d9eed04c756b2

--- js/maps-viewer.js
+++ js/maps-viewer.js
@@ -1,7 +1,7 @@
 (() => {
     // add the PMTiles plugin to the maplibregl global
-    let protocol = new pmtiles.Protocol({metadata: true});
-    maplibregl.addProtocol("pmtiles", protocol.tile);
+    //let protocol = new pmtiles.Protocol({metadata: true});
+    //maplibregl.addProtocol("pmtiles", protocol.tile);
 
     // Map initialization with default style
     const defaultStyle = 'https://betagouv.github.io/styles-de-cartes/maps/map_simplified_colored_ign.json';
