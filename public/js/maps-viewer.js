84e70f7b796e1f87d210266d43e29c2603d69fcb
Romain Ronflette <contact@romainronflette.com>
2025-04-22 11:47:36 +0200

    Add map position in url hash

--- public/js/maps-viewer.js
+++ public/js/maps-viewer.js
@@ -12,4 +12,13 @@
         const { styleData } = event.detail;
         map.setStyle(styleData);
     });
+
+    // Update the URL hash based on map state
+    setInterval(() => {
+        const currentHash = window.location.hash;
+        const newHash = `#map=${map.getZoom().toFixed(1)}/${map.getCenter().lat.toFixed(6)}/${map.getCenter().lng.toFixed(6)}`;
+        if (currentHash !== newHash) {
+            window.location.hash = newHash;
+        }
+    }, 100);
 })();
